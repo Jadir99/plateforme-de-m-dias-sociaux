9447b0cf6f3fc8454c8505b944c247fc61133c9f
JADIR Mohammed <104795363+Jadir99@users.noreply.github.com>
2024-04-21 18:15:54 +0100

    first store apicontroller its harde to think about logic

--- app/Http/Controllers/api/hashtagApiConntroller.php
+++ app/Http/Controllers/api/hashtagApiConntroller.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\hashtag;
+use Dotenv\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 
 class hashtagApiConntroller extends Controller
 {
@@ -20,7 +23,19 @@ class hashtagApiConntroller extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = FacadesValidator::make($request->all(), [
+            'hashtag_id' => 'required|Integer',
+            'hashtag_text' => 'required|string',
+        ]);
+        if ($validator->fails()){
+            return response()->json(['error' =>"error"], 400);
+        }
+        // creat new hashtag
+        $hashtag=new hashtag();
+        $hashtag->hashtag_id=$request->input('hashtag_id');
+        $hashtag->hashtag=$request->input('hashtag_text');
+        $hashtag->save();
+        return response()->json(['message' => 'hshtag created successfully'], 201);
     }
 
     /**
--- app/Http/Controllers/api/posteApiConntroller.php
+++ app/Http/Controllers/api/posteApiConntroller.php
@@ -3,17 +3,51 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\hashtag;
+use App\Models\hashtag_poste;
 use App\Models\poste;
 use Carbon\Carbon;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+use function PHPUnit\Framework\isEmpty;
+
 class posteApiConntroller extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
+
+    // private function for hashtags 
+    function extractHashtags($text)
+{
+    // Regular expression to match hashtags
+    $pattern = '/#(\w+)/';
+
+    // Array to store extracted hashtags
+    $hashtags = [];
+
+    // Extract hashtags from the text using regular expression
+    preg_match_all($pattern, $text, $matches);
+
+    // Store the extracted hashtags in the array
+    if (!empty($matches[1])) {
+        $hashtags = $matches[1];
+    }
+
+    return $hashtags;
+}
+// function to remaove hashtags from poste text :
+private function removeHashtags($text)
+{
+    // Regular expression to match hashtags
+    $pattern = '/#(\w+)/';
+
+    // Remove hashtags from the text using regular expression
+    $textWithoutHashtags = preg_replace($pattern, '', $text);
+
+    return $textWithoutHashtags;
+}
+
     public function index()
     {
         
@@ -32,16 +66,46 @@ class posteApiConntroller extends Controller
             'user_id' => 'required|Integer',
         ]);
 
+        $hashtags=$this->extractHashtags($request->input('text_poste'));
+
+        if ($hashtags!=NULL){
+            $hashtags_used=[];
+            // callfunction of insertion hashtag 
+            foreach ($hashtags as $hashtag){
+                
+                if (!hashtag::where('hashtag', $hashtag)->exists()){
+                    $newHashtag=new hashtag();
+                    $newHashtag->hashtag = $hashtag;
+                    $newHashtag->save();
+                    echo $newHashtag->id;
+                    array_push($hashtags_used,$newHashtag->id); 
+
+                }else {
+                    $hashtag=hashtag::where('hashtag', $hashtag)->firstOrFail();
+                    $hashtags_used[]=$hashtag->hashtag_id; 
+                    echo $hashtag->hashtag_id;
+                }
+            }
+        }
+        // l'ajout en joinre table 
+        foreach ($hashtags_used as $hashtag){
+            $new_hashtag_poste=new hashtag_poste();
+            $new_hashtag_poste->date_ajout= Carbon::now();
+            $new_hashtag_poste->id_poste=$request->input('user_id');
+            $new_hashtag_poste->hashtag_id= $hashtag;
+            $new_hashtag_poste->save();
+        }
         if ($validator->fails()) {
             return response()->json(['error' =>"kjnkjnjnkjn"], 400);
         }
 
         $poste = new poste();
         // $poste->id_poste = $request->input('id_poste');
-        $poste->text_poste = $request->input('text_poste');
+        $poste->text_poste = ($request->input('text_poste'));
         $poste->user_id = $request->input('user_id');
         $poste->Date_creation = Carbon::now();
         $poste->save();
+
 
         return response()->json(['message' => 'poste created successfully'], 201);
     }
